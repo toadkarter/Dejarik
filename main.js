3918c304d7ecd8f8f0c880a70e6c628b668484c5
Vlad Rakhmanin <vladimir.rakhmanin@ucdconnect.ie>
2022-09-28 20:26:01 +0100

    fixed issue with stats

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
+const inuStats = {
+    attack: 5,
+    defence: 3,
+}
+
+const skullStats = {
+    attack: 3,
+    defence: 5,
+}
+
 AFRAME.registerComponent('markerhandler', {
     init: function() {
+        const currentMarker = document.querySelector("#coolMarker");
         const model = document.querySelector("#inu");
         const overlay = document.querySelector('#points');
 
+        model.stats = inuStats;
+
         model.addEventListener('click', function(ev, target) {
-            const currentVisibility = overlay.getAttribute('visible');
-            overlay.setAttribute('visible', !currentVisibility);
+            const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+            console.log(intersectedElement);
+            if (model && intersectedElement == model) {
+                console.log(model.stats["attack"]);
+            }
+
+
+            // const currentVisibility = overlay.getAttribute('visible');
+            // overlay.setAttribute('visible', !currentVisibility);
         })
     }
 })
